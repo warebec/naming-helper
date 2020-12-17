d0cf04c0d0d39dcfa19edaa695b153cc6af8f329
warebec <69767312+warebec@users.noreply.github.com>
2020-12-17 14:03:45 -0500

    more logging

--- src/java/NamingHelperMain.java
+++ src/java/NamingHelperMain.java
@@ -63,6 +63,9 @@ public class NamingHelperMain {
     for (int i = 0; i < namesList.size(); i++) {
       Name firstName = namesList.get(i);
       if (!firstName.onlyLast) {
+        if ((num % 10) == 0) {
+          System.out.println("Adding names beginning with " + firstName.name);
+        }
         for (int j = 0; j < namesList.size(); j++) {
           if (i != j) {
             Name middleName1 = namesList.get(j);
@@ -75,7 +78,9 @@ public class NamingHelperMain {
                       if (i != l && j != l && k != l) {
                         Name lastName = namesList.get(k);
                         fullNamesList.add(new FullName(firstName, middleName1, middleName2, lastName));
-                        System.out.println("Added name #" + num++);
+                        if ((num % 10000) == 0) {
+                          System.out.println("Added name #" + num++);
+                        }
                       }
                     }
                   }
