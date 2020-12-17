4ef1db2409cc691e75d3b7e37f62246429c27c1b
warebec <warebec@gmail.com>
2020-12-16 19:41:31 -0500

    Codeanywhere commits

---
+++ src/java/FullName.java
@@ -0,0 +1,9 @@
+public class FullName {
+  String fullName;
+  String originAndMeaning;
+  
+  @Override
+    public String toString() { 
+        return fullName + " ::: " + originAndMeaning; 
+    }
+}
