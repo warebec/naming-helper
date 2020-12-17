49f895c63dab1aaea377962acf5b36e1ee0734ed
warebec <69767312+warebec@users.noreply.github.com>
2020-12-16 21:34:09 -0500

    attempting to recreate what I lost by not pushing

--- src/java/NamingHelperMain.java
+++ src/java/NamingHelperMain.java
@@ -1,10 +1,108 @@
+import java.io.*;
+import java.util.*;
+
 public class NamingHelperMain {
   public void main(String[] args) {
     // first, create List of names from namesList.csv
+    List<Name> namesList = new ArrayList<>();
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("namesList.csv")))) {
+      String line = "";
+      String csvBreakBy = ",";
+      int count = 0;
+      
+      while ((line = bufferedReader.readLine()) != null && count < 50) {
+        String[] nameProperties = line.split(csvBreakBy);
+        
+        while (!("true".equals(nameProperties[3] || "false".equals(nameProperties[3]))) {
+          nameProperties[2] = nameProperties[2] + ", " + nameProperties[3];
+          int index = 4;
+          while (index < nameProperties.length) {
+            nameProperties[index-1] = nameProperties[index];
+            index++;
+          }
+        }
+        
+        boolean onlyLast = false;
+        if ("true".equals(nameProperties[3]) {
+          onlyLast = true;
+        }
+        namesList.add(new Name(nameProperties[0], nameProperties[1], nameProperties[2], onlyLast));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
     // next, create List of full names based on that list
-    //   - 3 and 4 name combinations, only those with onlyLast=true being used only as surnames
+    List<FullName> fullNamesList = new ArrayList<>();
+            
+    // 3 name combinations
+    for (int i = 0; i < namesList.size(); i++) {
+      Name firstName = namesList.get(i);
+      if (!firstName.onlyLast) {
+        for (int j = 0; j < namesList.size(); j++) {
+          if (i != j) {
+            Name middleName = namesList.get(j);
+            if (!middleName.onlyLast) {
+              for (int k = 0; k < namesList.size(); k++) {
+                if (i != k && j != k) {
+                  Name lastName = namesList.get(k);
+                  fullNamesList.add(new FullName(firstName, middleName, lastName));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+            
+    // 4 name combinations
+    for (int i = 0; i < namesList.size(); i++) {
+      Name firstName = namesList.get(i);
+      if (!firstName.onlyLast) {
+        for (int j = 0; j < namesList.size(); j++) {
+          if (i != j) {
+            Name middleName1 = namesList.get(j);
+            if (!middleName1.onlyLast) {
+              for (int k = 0; k < namesList.size(); k++) {
+                if (i != k && j != k) {
+                  Name middleName2 = namesList.get(k);
+                  if (!middleName2.onlyLast) {
+                    for (int l = 0; l < namesList.size(); l++) {
+                      if (i != l && j != l && k != l) {
+                        Name lastName = namesList.get(k);
+                        fullNamesList.add(new FullName(firstName, middleName1, middleName2, lastName));
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    
     // next, shuffle the full names list to somewhat randomize the output
+    Collections.suffle(fullNamesList);
+            
     // finally, print full names to output.txt in format:
     //   - full name ::: origin and meaning of name
+    File output = new File("output.txt");
+    try {
+      if (output.exists()) {
+        output.delete();
+      }
+      output.createNewFile();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+         
+    try (FileWriter fileWriter = new FileWriter(output)) {
+      for (FullName fullName : fullNamesList) {
+        fileWriter.write(fullName.toString() + "\n");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
