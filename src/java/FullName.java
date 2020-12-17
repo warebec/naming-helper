a17d8c0eb788562f7b703101476e488c97e7c4e9
warebec <69767312+warebec@users.noreply.github.com>
2020-12-16 21:37:05 -0500

    trying to recover from lost push

--- src/java/FullName.java
+++ src/java/FullName.java
@@ -2,6 +2,16 @@ public class FullName {
   String fullName;
   String originAndMeaning;
   
+  FullName(firstName, middleName, lastName) {
+    this.fullName = firstName.name + " " + middleName.name + " " + lastName.name;
+    this.originAndMeaning = firstName.originAndMeaning + ", " + middleName.originAndMeaning + ", " + lastName.originAndMeaning;
+  }
+  
+  FullName(firstName, middleName1, middleName2, lastName) {
+    this.fullName = firstName.name + " " + middleName1.name + " " + middleName2.name + " " + lastName.name;
+    this.originAndMeaning = firstName.originAndMeaning + ", " + middleName1.originAndMeaning + ", " + middleName2.originAndMeaning + ", " + lastName.originAndMeaning;
+  }
+  
   @Override
     public String toString() { 
         return fullName + " ::: " + originAndMeaning; 
