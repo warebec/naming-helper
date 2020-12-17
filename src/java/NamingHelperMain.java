9218b981e7549d3b4c7457407b8bcc376803bcf3
warebec <warebec@gmail.com>
2020-12-16 19:37:10 -0500

    Codeanywhere commits

---
+++ src/java/NamingHelperMain.java
@@ -0,0 +1,10 @@
+public class NamingHelperMain {
+  public void main(String[] args) {
+    // first, create List of names from namesList.csv
+    // next, create List of full names based on that list
+    //   - 3 and 4 name combinations, only those with onlyLast=true being used only as surnames
+    // next, shuffle the full names list to somewhat randomize the output
+    // finally, print full names to output.txt in format:
+    //   - full name ::: origin and meaning of name
+  }
+}
