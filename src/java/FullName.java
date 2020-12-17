965a03c540252272a1363696f57418da681c4ca4
warebec <69767312+warebec@users.noreply.github.com>
2020-12-17 13:16:17 -0500

    fixing compile errors

--- src/java/FullName.java
+++ src/java/FullName.java
@@ -2,12 +2,12 @@ public class FullName {
   String fullName;
   String originAndMeaning;
   
-  FullName(firstName, middleName, lastName) {
+  FullName(Name firstName, Name middleName, Name lastName) {
     this.fullName = firstName.name + " " + middleName.name + " " + lastName.name;
     this.originAndMeaning = firstName.originAndMeaning + ", " + middleName.originAndMeaning + ", " + lastName.originAndMeaning;
   }
   
-  FullName(firstName, middleName1, middleName2, lastName) {
+  FullName(Name firstName, Name middleName1, Name middleName2, Name lastName) {
     this.fullName = firstName.name + " " + middleName1.name + " " + middleName2.name + " " + lastName.name;
     this.originAndMeaning = firstName.originAndMeaning + ", " + middleName1.originAndMeaning + ", " + middleName2.originAndMeaning + ", " + lastName.originAndMeaning;
   }
